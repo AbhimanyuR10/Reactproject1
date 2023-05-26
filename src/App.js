ea3626276123f1d0b1195dad6fd3b14b22bf43f6
swinstall <swinstall@VJC.COM>
2023-05-26 09:59:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import { TextField, Typography } from '@mui/material';
-import First from './components/First';
 import Navbar from './components/Navbar';
-import Statebasics from './components/Statebasics';
-import Buttons from './components/Buttons';
-import Table2 from './components/Table2';
 import { Route, Routes } from 'react-router-dom';
-import ApiGet from './components/ApiGet';
+import Student from './components/Student';
+
+import Add from './components/Add';
 
 function App() {
+
+ 
+
+
   return (
-    <div className="App">
-      <Navbar/>
-  <Routes>
-    <Route path='/' element={<Statebasics/>}/>
-    <Route path='/Table2' element={<Table2/>}/>
-    <Route path='/First' element={<First/>}/>
-    <Route path='/axios' element={<ApiGet/>}/>
-  </Routes>
-    </div>
+    <><Navbar /><div className="App">
+
+      <Routes>
+        <Route path='/' element={<Student />}></Route>
+        <Route path='/Add' element={<Add />}></Route>
+      </Routes>
+
+
+    </div></>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
-import { red } from '@mui/material/colors'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <AppBar>
-            <Toolbar>
-                <Typography color={'black'} sx={{ mr: 1 }} align='left'> New App</Typography>
-                <button><Link to={'/'}>Home</Link></button>
-                <button variant='text'><Link to='/Table2'>Table</Link></button>
-                <button variant='text'><Link to='/First'>First</Link></button>
-                <button variant='text'><Link to='/axios'>Axios</Link></button>
-            </Toolbar>
-        </AppBar>
+    <div >
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" style={{background:"lightgreen"}}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>ReactServer</Typography>
+                    <Button variant="contained" color="warning" ><Link to = {'/'} style = {{textDecoration:"none",color:"white"}}>STUDENT</Link></Button>
+                    <Button variant="contained" color="secondary"><Link to = {'/Add'} style = {{textDecoration:"none",color:"white"}}>ADD</Link></Button>
+                </Toolbar>
+
+            </AppBar>
+      
+        </Box>
     </div>
   )
 }
